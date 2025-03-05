66fbe80207fb45feb03e4299f3593f65e8314f7c
ZmolZito <maclucas07@hotmail.com>
2025-03-05 22:01:04 +0100

    Added
    VoteService
    ContentService

--- AspireHost/AspireHost.AppHost/Program.cs
+++ AspireHost/AspireHost.AppHost/Program.cs
@@ -1,5 +1,6 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
 var contentService = builder.AddProject<Projects.ContentService_Api>("contentservice");
+var voteService = builder.AddProject<Projects.VoteService_Api>("voteservice");
 
 builder.Build().Run();
