a317f27e3a441bb077190e32e73584325094ddb5
ZmolZito <maclucas07@hotmail.com>
2025-03-06 23:34:14 +0100

    Added - SubscriptionService

--- AspireHost/AspireHost.AppHost/Program.cs
+++ AspireHost/AspireHost.AppHost/Program.cs
@@ -1,6 +1,9 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
 var contentService = builder.AddProject<Projects.ContentService_Api>("contentservice");
+
 var voteService = builder.AddProject<Projects.VoteService_Api>("voteservice");
+
+var subscriptionService = builder.AddProject<Projects.SubscriptionService_Api>("subscriptionservice");
 
 builder.Build().Run();
