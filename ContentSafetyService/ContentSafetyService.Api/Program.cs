2f8f513d6d76f04bccb5523fe0ea07a8f0987639
Bilal Kinali <biki59986@edu.ucl.dk>
2025-03-10 13:21:04 +0100

    Added ContentSafetyService + AppSettings -> Azure secrets

---
+++ ContentSafetyService/ContentSafetyService.Api/Program.cs
@@ -0,0 +1,36 @@
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAspire", builder =>
+    {
+        builder.WithOrigins("https://localhost:7293")
+            .WithOrigins("http://localhost:5012")
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+
+    app.MapScalarApiReference();
+}
+
+//app.UseHttpsRedirection(); 
+
+app.UseCors("AllowAspire");
+
+app.MapGet("/hello", () => "Hello World!");
+
+app.Run();
