c200c46632cbd58958c35f6c66ffc1078c48d4fd
Bilal Kinali <biki59986@edu.ucl.dk>
2025-03-27 13:34:43 +0100

    some cleanup

--- AspireHost/AspireHost.AppHost/Program.cs
+++ AspireHost/AspireHost.AppHost/Program.cs
@@ -1,11 +1,9 @@
 using CommunityToolkit.Aspire.Hosting.Dapr;
 
-Environment.SetEnvironmentVariable("DAPR_COMPONENTS_PATH", Path.Combine(Directory.GetCurrentDirectory(), "components"));
-
 var builder = DistributedApplication.CreateBuilder(args);
 
-var stateStore = builder.AddDaprStateStore("statestore");
-var pubSub = builder.AddDaprPubSub("pubsub");
+//var stateStore = builder.AddDaprStateStore("statestore");
+//var pubSub = builder.AddDaprPubSub("pubsub");
 
 var contentService = builder.AddProject<Projects.ContentService_Api>("contentservice");
 
@@ -13,9 +11,9 @@ var voteService = builder.AddProject<Projects.VoteService_Api>("voteservice");
 
 var subscriptionService = builder.AddProject<Projects.SubscriptionService_Api>("subscriptionservice");
 
-var contentSafetyService = builder.AddProject<Projects.ContentSafetyService_Api>("contentsafetyservice")
-    .WithDaprSidecar()
-    .WithReference(stateStore)
-    .WithReference(pubSub);
+var contentSafetyService = builder.AddProject<Projects.ContentSafetyService_Api>("contentsafetyservice");
+    //.WithDaprSidecar()
+    //.WithReference(stateStore)
+    //.WithReference(pubSub);
 
 builder.Build().Run();
